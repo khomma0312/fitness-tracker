abebf6f799b7868d3619cdad551e59733936f710
khomma0312 <kijikuroneko@gmail.com>
2024-06-16 00:16:00 +0900

    [change]サイドバー追加に伴って、全体のレイアウトを修正

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "@/layouts/Header";
+import { Sidebar } from "./layouts/Sidebar";
 
 export const Layout = () => {
   return (
-    <div>
+    <div className="app-layout">
       <Header />
+      <Sidebar />
       <Outlet />
     </div>
   );
