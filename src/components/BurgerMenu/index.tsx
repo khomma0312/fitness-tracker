8875a931484a9c21e1bbb806bce97929e0bf9673
khomma0312 <kijikuroneko@gmail.com>
2024-06-16 17:04:43 +0900

    [change]バーガーメニューコンポーネントの構造を変更

--- src/components/BurgerMenu/index.tsx
+++ src/components/BurgerMenu/index.tsx
@@ -1,18 +1,8 @@
-import { Burger, Menu } from "@mantine/core";
+import { Burger } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
-import { MenuDropdownItems } from "../MenuDropdownItems";
 
 export const BurgerMenu = () => {
   const [isMenuOpened, { toggle }] = useDisclosure(false);
 
-  return (
-    <Menu opened={isMenuOpened}>
-      <Menu.Target>
-        <Burger color="brand-blue.7" opened={isMenuOpened} onClick={toggle} />
-      </Menu.Target>
-      <Menu.Dropdown>
-        <MenuDropdownItems />
-      </Menu.Dropdown>
-    </Menu>
-  );
+  return <Burger color="brand-blue.7" opened={isMenuOpened} onClick={toggle} />;
 };
