2d199b0003e50cc27d84c5893dd0cd4d8733c34c
khomma0312 <kijikuroneko@gmail.com>
2024-06-16 17:05:42 +0900

    [change]サイドバーのスマホ用レイアウトに適応するよう修正

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,12 +1,19 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "@/layouts/Header";
 import { Sidebar } from "./layouts/Sidebar";
+import { WhenVisible } from "./components/WhenVisible";
+import { TopDrawer } from "./components/TopDrawer";
 
 export const Layout = () => {
   return (
     <div className="app-layout">
       <Header />
-      <Sidebar />
+      <WhenVisible lg xl>
+        <Sidebar />
+      </WhenVisible>
+      <WhenVisible sm md>
+        <TopDrawer />
+      </WhenVisible>
       <Outlet />
     </div>
   );
