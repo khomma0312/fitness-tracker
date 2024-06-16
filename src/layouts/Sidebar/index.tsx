bfcf083bf8b24377c535b168e54b4a7a869bfc55
khomma0312 <kijikuroneko@gmail.com>
2024-06-16 17:03:12 +0900

    [change]サイドバーのアイテムをループで回すよう修正

--- src/layouts/Sidebar/index.tsx
+++ src/layouts/Sidebar/index.tsx
@@ -6,30 +6,35 @@ import { HiOutlineUserCircle } from "react-icons/hi";
 import styles from "./styles.module.css";
 import { paths } from "@/services/utils/paths";
 
+const sidebarItems = [
+  {
+    icon: <HiOutlineHome size={22} />,
+    title: "ホーム",
+    linkTo: paths.dashboard,
+  },
+  {
+    icon: <HiOutlineSearch size={22} />,
+    title: "トレーニングを探す",
+    linkTo: paths.workouts,
+  },
+  {
+    icon: <HiOutlineViewList size={22} />,
+    title: "トレーニング履歴",
+    linkTo: paths.history,
+  },
+  {
+    icon: <HiOutlineUserCircle size={22} />,
+    title: "マイページ",
+    linkTo: paths.mypage,
+  },
+];
+
 export const Sidebar = () => {
   return (
     <aside className={styles.sidebar}>
-      {/* TODO: パスはcontextとかで共通で持つようにして、そこから取得するようにする */}
-      <SideBarItem
-        icon={<HiOutlineHome size={22} />}
-        title="ホーム"
-        linkTo={paths.dashboard}
-      />
-      <SideBarItem
-        icon={<HiOutlineSearch size={22} />}
-        title="トレーニングを探す"
-        linkTo={paths.workouts}
-      />
-      <SideBarItem
-        icon={<HiOutlineViewList size={22} />}
-        title="トレーニング履歴"
-        linkTo={paths.history}
-      />
-      <SideBarItem
-        icon={<HiOutlineUserCircle size={22} />}
-        title="マイページ"
-        linkTo={paths.mypage}
-      />
+      {sidebarItems.map((item) => (
+        <SideBarItem icon={item.icon} title={item.title} linkTo={item.linkTo} />
+      ))}
     </aside>
   );
 };
