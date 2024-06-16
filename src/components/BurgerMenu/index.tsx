bd3d274ac865d32bae8315a6b466a12e5e0209e5
khomma0312 <kijikuroneko@gmail.com>
2024-06-16 22:55:41 +0900

    [change]スマホ用メニューにログアウトの項目も追加

--- src/components/BurgerMenu/index.tsx
+++ src/components/BurgerMenu/index.tsx
@@ -1,8 +1,41 @@
-import { Burger } from "@mantine/core";
+import { Burger, Drawer, UnstyledButton } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
+import { menuItems } from "@/services/utils/menuItems";
+import { HiOutlineHome } from "react-icons/hi";
+import { HiOutlineSearch } from "react-icons/hi";
+import { HiOutlineViewList } from "react-icons/hi";
+import { HiOutlineUserCircle } from "react-icons/hi";
+import { SideBarItem } from "../SideBarItem";
+import styles from "./styles.module.css";
+
+const drawerItems = [
+  { ...menuItems.dashboard, icon: <HiOutlineHome size={22} /> },
+  { ...menuItems.workouts, icon: <HiOutlineSearch size={22} /> },
+  { ...menuItems.history, icon: <HiOutlineViewList size={22} /> },
+  { ...menuItems.mypage, icon: <HiOutlineUserCircle size={22} /> },
+];
 
 export const BurgerMenu = () => {
-  const [isMenuOpened, { toggle }] = useDisclosure(false);
+  const [isMenuOpened, { toggle, close }] = useDisclosure(false);
 
-  return <Burger color="brand-blue.7" opened={isMenuOpened} onClick={toggle} />;
+  return (
+    <>
+      <Burger color="brand-blue.7" opened={isMenuOpened} onClick={toggle} />
+      <Drawer opened={isMenuOpened} onClose={close}>
+        <ul>
+          {drawerItems.map((item) => (
+            <SideBarItem
+              key={item.linkTo}
+              icon={item.icon}
+              title={item.title}
+              linkTo={item.linkTo}
+            />
+          ))}
+          <li className={styles.logout}>
+            <UnstyledButton>ログアウト</UnstyledButton>
+          </li>
+        </ul>
+      </Drawer>
+    </>
+  );
 };
