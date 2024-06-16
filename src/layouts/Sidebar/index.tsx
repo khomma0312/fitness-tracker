ab5f6f1f0c10a7d074eba6903c95681d288d2cfa
khomma0312 <kijikuroneko@gmail.com>
2024-06-16 10:46:34 +0900

    [change]定数を使ってルーティングを定義するよう修正

--- src/layouts/Sidebar/index.tsx
+++ src/layouts/Sidebar/index.tsx
@@ -4,31 +4,32 @@ import { HiOutlineSearch } from "react-icons/hi";
 import { HiOutlineViewList } from "react-icons/hi";
 import { HiOutlineUserCircle } from "react-icons/hi";
 import styles from "./styles.module.css";
+import { paths } from "@/services/utils/paths";
 
 export const Sidebar = () => {
   return (
-    <div className={styles.sidebar}>
+    <aside className={styles.sidebar}>
       {/* TODO: パスはcontextとかで共通で持つようにして、そこから取得するようにする */}
       <SideBarItem
         icon={<HiOutlineHome size={22} />}
         title="ホーム"
-        linkTo="/"
+        linkTo={paths.dashboard}
       />
       <SideBarItem
         icon={<HiOutlineSearch size={22} />}
         title="トレーニングを探す"
-        linkTo="/workouts"
+        linkTo={paths.workouts}
       />
       <SideBarItem
         icon={<HiOutlineViewList size={22} />}
         title="トレーニング履歴"
-        linkTo="/history"
+        linkTo={paths.history}
       />
       <SideBarItem
         icon={<HiOutlineUserCircle size={22} />}
         title="マイページ"
-        linkTo="/mypage"
+        linkTo={paths.mypage}
       />
-    </div>
+    </aside>
   );
 };
