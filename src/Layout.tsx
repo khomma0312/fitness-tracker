76f7fabf0d13be7d3167b4733ad6147179d71ea8
khomma0312 <kijikuroneko@gmail.com>
2024-06-16 22:36:23 +0900

    [change]レイアウトの変更

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -2,7 +2,6 @@ import { Outlet } from "react-router-dom";
 import { Header } from "@/layouts/Header";
 import { Sidebar } from "./layouts/Sidebar";
 import { WhenVisible } from "./components/WhenVisible";
-import { TopDrawer } from "./components/TopDrawer";
 
 export const Layout = () => {
   return (
@@ -10,9 +9,6 @@ export const Layout = () => {
       <Header />
       <WhenVisible lg xl>
         <Sidebar />
-      </WhenVisible>
-      <WhenVisible sm md>
-        <TopDrawer />
       </WhenVisible>
       <Outlet />
     </div>
