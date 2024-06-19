adf4ac5a1f373cada4ba14db28bdff525dd8bf9b
khomma0312 <kijikuroneko@gmail.com>
2024-06-20 00:05:21 +0900

    [change]Home画面のスタイルを変更

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,14 @@
+import { Card } from "@/components/Card";
+import styles from "./styles.module.css";
+import { WorkoutTimeTransitionChart } from "@/widgets/WorkoutTimeTransitionChart";
+
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className={styles.dashboard}>
+      <div className={styles.container}>
+        <WorkoutTimeTransitionChart />
+        <Card>テスト</Card>
+      </div>
+    </div>
+  );
 };
