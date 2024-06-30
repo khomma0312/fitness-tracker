842bb53fd9ece54f7243130e6eebb114fa5606d9
khomma0312 <kijikuroneko@gmail.com>
2024-06-30 23:45:46 +0900

    add: エラー処理用のコンポーネントを追加

---
+++ src/components/HandleServerError/index.tsx
@@ -0,0 +1,22 @@
+import { ErrorBoundary } from "react-error-boundary";
+import { ErrorMessage } from "../ErrorMessage";
+
+interface HandleServerErrorProps {
+  children: React.ReactNode;
+}
+
+export const ErrorFallback = () => {
+  return <ErrorMessage />;
+};
+
+const onError = (error: Error) => {
+  console.log("error.message", error.message);
+};
+
+export const HandleServerError = ({ children }: HandleServerErrorProps) => {
+  return (
+    <ErrorBoundary FallbackComponent={ErrorFallback} onError={onError}>
+      {children}
+    </ErrorBoundary>
+  );
+};
