40fa714727d2614b9611a34faf61431ae6d4861f
khomma0312 <kijikuroneko@gmail.com>
2024-06-30 23:55:47 +0900

    add: トレーニング時間をグラフ表示するコンポーネントを追加

---
+++ src/hooks/api/useWorkoutTimeHistory/index.ts
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { useErrorBoundary } from "react-error-boundary";
+import { getWorkoutTimeHistory } from "@/services/api/getWorkoutTimeHistory";
+
+export const useWorkoutTimeHistory = (
+  userId: number,
+  startYearMonth: string,
+  endYearMonth: string,
+) => {
+  const { showBoundary } = useErrorBoundary();
+
+  const { data, isLoading, error } = useSuspenseQuery({
+    queryKey: ["getWorkoutTimeHistory"],
+    queryFn: () => getWorkoutTimeHistory(userId, startYearMonth, endYearMonth),
+  });
+
+  useEffect(() => {
+    if (error) showBoundary(error);
+  }, [error, showBoundary]);
+
+  return { workoutTimes: data, isLoading, error };
+};
---
+++ src/services/api/getWorkoutTimeHistory/index.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+export interface WorkoutTime {
+  month: string;
+  time: number;
+}
+
+export const getWorkoutTimeHistory = async (
+  userId: number,
+  startYearMonth: string,
+  endYearMonth: string,
+) => {
+  const res = await axios.get<WorkoutTime[]>("/user/workout-time-history", {
+    params: {
+      userId,
+      startYearMonth,
+      endYearMonth,
+    },
+  });
+  return res.data;
+};
