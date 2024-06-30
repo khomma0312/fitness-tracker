9e820c8550b5caddb1d624ba8a2d93caecfb73a2
khomma0312 <kijikuroneko@gmail.com>
2024-06-30 23:57:37 +0900

    change: メインエリアをErrorBoundary、Suspenseでラップするよう修正

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -2,6 +2,9 @@ import { Outlet } from "react-router-dom";
 import { Header } from "@/layouts/Header";
 import { Sidebar } from "./layouts/Sidebar";
 import { WhenVisible } from "./components/WhenVisible";
+import { HandleServerError } from "./components/HandleServerError";
+import { Suspense } from "react";
+import { Loader } from "./components/Loader";
 
 export const Layout = () => {
   return (
@@ -10,7 +13,11 @@ export const Layout = () => {
       <WhenVisible lg xl>
         <Sidebar />
       </WhenVisible>
-      <Outlet />
+      <HandleServerError>
+        <Suspense fallback={<Loader />}>
+          <Outlet />
+        </Suspense>
+      </HandleServerError>
     </div>
   );
 };
