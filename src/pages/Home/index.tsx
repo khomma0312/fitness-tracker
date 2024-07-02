8b143f73066e74493b43a402ac20a66a783b1dc1
khomma0312 <kijikuroneko@gmail.com>
2024-07-03 00:20:15 +0900

    change: HomeにTodaysTrainingTableを追加、レイアウトを修正

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,14 @@
-import { Card } from "@/components/Card";
-import styles from "./styles.module.css";
 import { WorkoutTimeTransitionChart } from "@/widgets/WorkoutTimeTransitionChart";
+import styles from "./styles.module.css";
+import { TodaysTrainingTable } from "@/widgets/TodaysTrainingTable";
 
 export const Home = () => {
+  // TODO: Jotaiなどの状態管理ライブラリを使ってstoreからstartYearMonth, endYearMonth, dateを取得する
   return (
     <div className={styles.dashboard}>
       <div className={styles.container}>
-        <WorkoutTimeTransitionChart />
-        <Card>テスト</Card>
+        <WorkoutTimeTransitionChart startYearMonth="2021-03" endYearMonth="2021-08" />
+        <TodaysTrainingTable />
       </div>
     </div>
   );
